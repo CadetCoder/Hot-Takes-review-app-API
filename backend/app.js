925b3df9dfa1457f91aef3995e6a65a9fc81f3a7
Darrel Toledo <darreltoledo@icloud.com>
2020-06-22 22:18:29 +0100

    express app running

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://cadetCoder:bhQOa66y3q6lO1OF@cluster0-havww.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+//API route
+app.use((req, res, next) => {
+  // * allows any request from any origin will be allowed to access API
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('api/sauces', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Sauce created successfully!'
+  });
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+
+
+module.exports = app;
