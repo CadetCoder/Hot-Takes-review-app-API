cb3297e4eb3f8d3e71efc9da6fb7db216caa6910
Darrel Toledo <darreltoledo@icloud.com>
2020-06-23 21:52:58 +0100

    token's secret key changed to TOKEN_TOP_SECRET

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+
+    const decodedToken = jwt.verify(token, 'TOKEN_TOP_SECRET');
+    console.log(decodedToken);
     const userId = decodedToken.userId;
+    console.log(userId);
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
